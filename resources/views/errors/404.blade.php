f096d536bea0cf75920ab6464ad561dd39f06490
Ammer97 <ameerwisam97@gmail.com>
2019-04-08 07:08:03 +0300

    add custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.home')
+@section('home')
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-8">
+                <img src="/err/image404.png">
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/masterErr.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>Shopping</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <link rel="stylesheet" href="/css/grid.css">
+    <link rel="stylesheet" href="/home/assets/css/main.css">
+</head>
+<body class="is-preload">
+
+
+
+<div class="container">
+    <div class="row justify-content-center mt-5">
+        <div class="col-6">
+            @yield('err')
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!-- Scripts -->
+
+<script src="/home/assets/js/main.js"></script>
+</body>
+</html>
